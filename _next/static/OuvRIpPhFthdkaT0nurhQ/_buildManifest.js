93f1000b7f42c8ab4c5a1c29f490743126d903e1
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2020-07-05 20:14:03 +0100

    change endpoint

---
+++ _next/static/OuvRIpPhFthdkaT0nurhQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/airconditioning":[a],"/contact":[a],"/index":[a],"/services":[a],"/testimonials":[a]}}("static\u002Fchunks\u002Fbcf221c24f67a3a123ed87a743d770b73209b80a.c03bbfce2a79747a2c77.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/ruopel58kZ6hwON9lviCD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/airconditioning":[a],"/contact":[a],"/index":[a],"/services":[a],"/testimonials":[a]}}("static\u002Fchunks\u002F61741e154d8488c119e9445afd1c65ec8b347f0f.c03bbfce2a79747a2c77.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
